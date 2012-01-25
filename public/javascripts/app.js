0000f1f6366e608e4acc4591323f1325600cec22
Martin Stannard <martin.stannard@blake.com.au>
2012-01-25 15:39:24 +1100

    added new seamless background and tweaked speeds of parallax
    Test on 3 browsers - FF and Safari both render fonts ok, while Chrome
    fails to render both the framecounter and the words

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,46 +1,31 @@
 (function() {
-  var BUILDING_DENSITY_FACTOR, CEILING_LEVEL, Collider, FLOOR_LEVEL, FPS, GRASS_SPEED, GRASS_WIDTH, Game, GameState, Grass, Gravity, HEIGHT, INTERVAL, InitialLevelSpeed, JetpackThrust, KEYCODE_A, KEYCODE_D, KEYCODE_ESC, KEYCODE_LEFT, KEYCODE_RIGHT, KEYCODE_SPACE, KEYCODE_UP, KEYCODE_W, Logo, Obstacle, PLAYER_X_OFFSET, Player, SKY_SPEED, SKY_WIDTH, Sector, Sky, Stats, WIDTH, Word, building_dimensions, count, frames, height, i, num, offset, sparklesFrameData, spriteData, width, widths, words,
-    __hasProp = Object.prototype.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; },
-    __slice = Array.prototype.slice,
-    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-
+  var BUILDING_DENSITY_FACTOR, CEILING_LEVEL, Collider, FLOOR_LEVEL, FPS, GRASS_SPEED, GRASS_WIDTH, Game, GameState, Grass, Gravity, HEIGHT, INTERVAL, InitialLevelSpeed, JetpackThrust, KEYCODE_A, KEYCODE_D, KEYCODE_ESC, KEYCODE_LEFT, KEYCODE_RIGHT, KEYCODE_SPACE, KEYCODE_UP, KEYCODE_W, Logo, Obstacle, PLAYER_X_OFFSET, Player, SKY_SPEED, SKY_WIDTH, Sector, Sky, Stats, WIDTH, Word, building_dimensions, count, frames, height, i, num, offset, sparklesFrameData, spriteData, width, widths, words;
+  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+    function ctor() { this.constructor = child; }
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor;
+    child.__super__ = parent.prototype;
+    return child;
+  }, __slice = Array.prototype.slice, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   FPS = 60;
-
   INTERVAL = 1 / FPS * 1000;
-
   WIDTH = 600;
-
   HEIGHT = 400;
-
   FLOOR_LEVEL = HEIGHT - 3 - 32;
-
   CEILING_LEVEL = 10;
-
   Gravity = 275;
-
   JetpackThrust = -800;
-
-  SKY_WIDTH = 900;
-
-  SKY_SPEED = 0.2;
-
+  SKY_WIDTH = 2135;
+  SKY_SPEED = 1.4;
   GRASS_WIDTH = 600;
-
-  GRASS_SPEED = 8.0;
-
+  GRASS_SPEED = 4.0;
   BUILDING_DENSITY_FACTOR = 0.005;
-
   PLAYER_X_OFFSET = 100;
-
   widths = [29, 32, 29, 31, 31];
-
   building_dimensions = [[190, 265], [194, 259], [220, 220], [253, 204], [181, 132], [267, 200]];
-
   words = "FISH CAT HAT POO BUM RED BLUE        ENNUI DEPRESSION MORTGAGE        RUMPYPUMPY WHISKEY        ROBUST BUXOM WANTON INTERCOURSE";
-
   words = words.split(/\s+/);
-
   spriteData = {
     images: ["images/mario.png", "images/mariosplat.png"],
     animations: {
@@ -58,35 +43,24 @@
       }
     }
   };
-
   count = 0;
-
   spriteData.frames = [];
-
   width = 30;
-
   height = 16;
-
   offset = 0;
-
   for (i = 0; i < 12; i++) {
     spriteData.frames.push([offset, 0, width, height, 0]);
     offset += width;
     count += 1;
   }
-
   width = 32;
-
   height = 32;
-
   offset = 0;
-
   for (i = 0; i < 8; i++) {
     spriteData.frames.push([0, offset, width, height, 1]);
     offset += height;
     count += 1;
   }
-
   frames = (function() {
     var _results;
     _results = [];
@@ -96,9 +70,7 @@
     }
     return _results;
   })();
-
   spriteData.animations.splat.frames = _.flatten(frames);
-
   sparklesFrameData = {
     images: ["images/sparkle_21x23.png"],
     frames: {
@@ -108,11 +80,8 @@
       regY: 11
     }
   };
-
-  Player = (function(_super) {
-
-    __extends(Player, _super);
-
+  Player = (function() {
+    __extends(Player, Container);
     function Player(game) {
       this.game = game;
       Container.prototype.initialize.apply(this);
@@ -132,16 +101,13 @@
       this.score = 0;
       this.ticks = 0;
     }
-
     Player.prototype.finishedDying = function() {
       return this.game.fire('dead');
     };
-
     Player.prototype.addScore = function(score) {
       this.score += score;
       return this.addSparkle();
     };
-
     Player.prototype.makeAnim = function() {
       var player;
       this.spriteSheet = new SpriteSheet(spriteData);
@@ -150,10 +116,11 @@
       this.anim.gotoAndPlay('run');
       player = this;
       return this.anim.onAnimationEnd = function(anim, anim) {
-        if (anim === 'splat') return player.finishedDying();
+        if (anim === 'splat') {
+          return player.finishedDying();
+        }
       };
     };
-
     Player.prototype.drawFlame = function() {
       var g, o;
       this.flame = new Shape();
@@ -175,11 +142,9 @@
       g.lineTo(-4, -3);
       return g.lineTo(-2, -0);
     };
-
     Player.prototype.makeSparkles = function() {
       return this.bmpAnim = new BitmapAnimation(new SpriteSheet(sparklesFrameData));
     };
-
     Player.prototype.addSparkle = function() {
       var angle, sparkle, speed, v;
       sparkle = this.bmpAnim.clone();
@@ -194,11 +159,12 @@
       this.sparkles.push(sparkle);
       return this.addChild(sparkle);
     };
-
     Player.prototype.fire = function() {
       var args, event;
       event = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
-      if (this.state === 'die' || this.state === 'dying') return;
+      if (this.state === 'die' || this.state === 'dying') {
+        return;
+      }
       if (this.state !== event) {
         this.state = event;
         switch (this.state) {
@@ -212,17 +178,14 @@
         }
       }
     };
-
     Player.prototype.die = function() {
       this.state = 'dying';
       this.game.fire('dying');
       return this.anim.gotoAndPlay('splat');
     };
-
     Player.prototype.finishedDying = function() {
       return this.game.fire('dead');
     };
-
     Player.prototype.tick = function() {
       var accel, dt, newSparkles, sparkle, _i, _len, _ref;
       dt = INTERVAL / 1000;
@@ -235,7 +198,9 @@
         case 'dying':
           accel = Gravity / 2;
           this.flame.visible = false;
-          if (this.y <= 0) this.finishedDying();
+          if (this.y <= 0) {
+            this.finishedDying();
+          }
           break;
         default:
           accel = Gravity;
@@ -268,19 +233,12 @@
       }
       return this.sparkles = newSparkles;
     };
-
     Player.prototype.bumpedCeiling = function() {};
-
     Player.prototype.bumpedFloor = function() {};
-
     return Player;
-
-  })(Container);
-
+  })();
   Collider = (function() {
-
     function Collider() {}
-
     Collider.prototype.collide = function(player, colliders) {
       var collider, _i, _len, _results;
       _results = [];
@@ -289,19 +247,13 @@
         if (collider.contains(player)) {
           collider.hit(player);
           break;
-        } else {
-          _results.push(void 0);
         }
       }
       return _results;
     };
-
     return Collider;
-
   })();
-
   Stats = (function() {
-
     function Stats(stage) {
       this.fps = new Text("Hello again", "bold 12px Arial", "#00FF55");
       this.fps.x = 10;
@@ -314,43 +266,27 @@
       this.score.text = "Score";
       stage.addChild(this.score);
     }
-
     Stats.prototype.tick = function() {
       return this.fps.text = Ticker.getMeasuredFPS().toString().substring(0, 2);
     };
-
     return Stats;
-
   })();
-
-  Sky = (function(_super) {
-
-    __extends(Sky, _super);
-
+  Sky = (function() {
+    __extends(Sky, Bitmap);
     function Sky() {
       Bitmap.prototype.initialize.apply(this);
     }
-
     return Sky;
-
-  })(Bitmap);
-
-  Grass = (function(_super) {
-
-    __extends(Grass, _super);
-
+  })();
+  Grass = (function() {
+    __extends(Grass, Bitmap);
     function Grass() {
       Bitmap.prototype.initialize.apply(this);
     }
-
     return Grass;
-
-  })(Bitmap);
-
-  Obstacle = (function(_super) {
-
-    __extends(Obstacle, _super);
-
+  })();
+  Obstacle = (function() {
+    __extends(Obstacle, Bitmap);
     function Obstacle(image, x, y, width, height) {
       this.x = x;
       this.y = y;
@@ -358,31 +294,23 @@
       this.height = height;
       Bitmap.prototype.initialize.apply(this, [image]);
     }
-
     Obstacle.prototype.contains = function(t) {
       var x, y, _ref;
       _ref = t.localToLocal(0, 0, this), x = _ref.x, y = _ref.y;
       return x + t.width > 0 && y + t.height > 0;
     };
-
     Obstacle.prototype.is_collidable = function(p) {
       var x, y, _ref;
       _ref = p.localToLocal(0, 0, this), x = _ref.x, y = _ref.y;
       return x < this.width;
     };
-
     Obstacle.prototype.hit = function(player) {
       return player.die();
     };
-
     return Obstacle;
-
-  })(Bitmap);
-
-  Word = (function(_super) {
-
-    __extends(Word, _super);
-
+  })();
+  Word = (function() {
+    __extends(Word, Text);
     function Word(word, x, y) {
       this.x = x;
       this.y = y;
@@ -393,32 +321,25 @@
       this.height = this.getMeasuredLineHeight();
       this.y -= this.height;
     }
-
     Word.prototype.tick = function() {
-      if (this.wasHit) return this.color = '#400';
+      if (this.wasHit) {
+        return this.color = '#400';
+      }
     };
-
     Word.prototype.contains = function(t) {
       var x, y, _ref;
       _ref = t.localToLocal(0, 0, this), x = _ref.x, y = _ref.y;
       return x + t.width > 0 && y + t.height > -this.height && y < this.height;
     };
-
     Word.prototype.hit = function(player) {
       player.addScore(100);
       return this.wasHit = true;
     };
-
     return Word;
-
-  })(Text);
-
+  })();
   InitialLevelSpeed = 2.5;
-
-  Sector = (function(_super) {
-
-    __extends(Sector, _super);
-
+  Sector = (function() {
+    __extends(Sector, Container);
     function Sector(game) {
       this.game = game;
       Container.prototype.initialize.apply(this);
@@ -428,10 +349,11 @@
       this.next_building_jitter = 200;
       this.obstacle();
     }
-
     Sector.prototype.tick = function() {
       var child, child_count, i;
-      if (this.stopped) return;
+      if (this.stopped) {
+        return;
+      }
       this.remove_children();
       if (!this.game.dead) {
         this.generate();
@@ -448,12 +370,12 @@
         }
       }
     };
-
     Sector.prototype.generate = function() {
-      if (this.next_building_time <= 0) this.obstacle();
+      if (this.next_building_time <= 0) {
+        this.obstacle();
+      }
       return this.next_building_time -= 1;
     };
-
     Sector.prototype.obstacle = function() {
       var bitmap, image, x, y, _ref;
       image = Math.floor(Math.random() * building_dimensions.length);
@@ -464,10 +386,11 @@
       this.addChild(bitmap);
       this.next_building_time = bitmap.width + Math.random() * this.next_building_jitter;
       this.next_building_jitter -= 2.0;
-      if (this.next_building_jitter < 30) this.next_building_jitter = 30;
+      if (this.next_building_jitter < 30) {
+        this.next_building_jitter = 30;
+      }
       return this.word(bitmap);
     };
-
     Sector.prototype.word = function(obstacle) {
       var text, word, x_pos;
       text = words[Math.floor(Math.random() * words.length)];
@@ -475,10 +398,11 @@
       word = new Word(text, x_pos, obstacle.y);
       return this.addChild(word);
     };
-
     Sector.prototype.remove_children = function() {
       var abs_x, child;
-      if (this.getNumChildren() === 0) return;
+      if (this.getNumChildren() === 0) {
+        return;
+      }
       child = this.getChildAt(0);
       abs_x = this.x + child.x + child.width;
       if (abs_x < 0) {
@@ -486,92 +410,66 @@
         return this.removeChildAt(0);
       }
     };
-
     return Sector;
-
-  })(Container);
-
+  })();
   KEYCODE_SPACE = 32;
-
   KEYCODE_UP = 38;
-
   KEYCODE_LEFT = 37;
-
   KEYCODE_RIGHT = 39;
-
   KEYCODE_W = 87;
-
   KEYCODE_A = 65;
-
   KEYCODE_D = 68;
-
   KEYCODE_ESC = 27;
-
   GameState = (function() {
-
     function GameState(stage) {
       this.stage = stage;
       this.handleKeyUp = __bind(this.handleKeyUp, this);
       this.handleKeyDown = __bind(this.handleKeyDown, this);
     }
-
     GameState.prototype.enter = function() {
       $(document).keyup(this.handleKeyUp);
       return $(document).keydown(this.handleKeyDown);
     };
-
     GameState.prototype.exit = function() {
       $(document).unbind('keyup', this.handleKeyUp);
       $(document).unbind('keydown', this.handleKeyDown);
       return this.stage.removeAllChildren();
     };
-
     GameState.prototype.tick = function() {
       return this.stage.update();
     };
-
     GameState.prototype.changeState = function(state) {
       this.exit();
       Ticker.removeListener(this);
       state.enter();
       return Ticker.addListener(state);
     };
-
     GameState.prototype.handleKeyDown = function() {
       return console.log("base kd");
     };
-
     GameState.prototype.handleKeyUp = function() {
       return console.log("base ku");
     };
-
     return GameState;
-
   })();
-
-  Logo = (function(_super) {
-
-    __extends(Logo, _super);
-
+  Logo = (function() {
+    __extends(Logo, GameState);
     function Logo(stage, game) {
       this.stage = stage;
       this.game = game;
       this.handleKeyUp = __bind(this.handleKeyUp, this);
       Logo.__super__.constructor.apply(this, arguments);
     }
-
     Logo.prototype.enter = function() {
-      var _this = this;
       Logo.__super__.enter.apply(this, arguments);
       this.logo = new Bitmap("images/logo.jpg");
       this.stage.addChild(this.logo);
       this.stage.clearEvents();
-      return this.stage.onClick = function() {
+      return this.stage.onClick = __bind(function() {
         console.log("onclick in logo");
-        return _this.changeState(_this.game);
-      };
+        return this.changeState(this.game);
+      }, this);
     };
-
     Logo.prototype.handleKeyUp = function(e) {
       e.stopPropagation();
       switch (e.keyCode) {
@@ -579,15 +477,10 @@
           return this.changeState(this.game);
       }
     };
-
     return Logo;
-
-  })(GameState);
-
-  Game = (function(_super) {
-
-    __extends(Game, _super);
-
+  })();
+  Game = (function() {
+    __extends(Game, GameState);
     function Game(stage) {
       this.stage = stage;
       this.handleKeyUp = __bind(this.handleKeyUp, this);
@@ -595,25 +488,22 @@
       this.collider = new Collider;
       this.state = 'init';
     }
-
     Game.prototype.enter = function() {
-      var _this = this;
       Game.__super__.enter.apply(this, arguments);
       this.stage.clearEvents();
       this.start_game();
-      this.stage.onMouseDown = function() {
-        switch (_this.state) {
+      this.stage.onMouseDown = __bind(function() {
+        switch (this.state) {
           case 'running':
-            return _this.player.fire('jump');
+            return this.player.fire('jump');
           case 'dead':
-            return _this.changeState(_this);
+            return this.changeState(this);
         }
-      };
-      return this.stage.onMouseUp = function() {
-        return _this.player.fire('unjump');
-      };
+      }, this);
+      return this.stage.onMouseUp = __bind(function() {
+        return this.player.fire('unjump');
+      }, this);
     };
-
     Game.prototype.fire = function() {
       var args, event;
       event = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
@@ -624,7 +514,6 @@
           return this.state = 'dying';
       }
     };
-
     Game.prototype.handleKeyDown = function(e) {
       e.stopPropagation();
       e.preventDefault();
@@ -642,7 +531,6 @@
           return Ticker.setPaused(this.paused);
       }
     };
-
     Game.prototype.handleKeyUp = function(e) {
       e.stopPropagation();
       e.preventDefault();
@@ -651,7 +539,6 @@
           return this.player.fire('unjump');
       }
     };
-
     Game.prototype.tick = function() {
       switch (this.state) {
         case 'dying':
@@ -676,7 +563,6 @@
       }
       return this.stage.update();
     };
-
     Game.prototype.start_game = function() {
       this.state = 'starting';
       this.game_over = false;
@@ -700,25 +586,28 @@
       this.grass2.x += GRASS_WIDTH;
       return this.state = 'running';
     };
-
     Game.prototype.check_sky = function() {
       this.sky1.x -= SKY_SPEED;
       this.sky2.x -= SKY_SPEED;
-      if (this.sky1.x < -SKY_WIDTH) this.sky1.x += SKY_WIDTH * 2;
-      if (this.sky2.x < -SKY_WIDTH) return this.sky2.x += SKY_WIDTH * 2;
+      if (this.sky1.x < -SKY_WIDTH) {
+        this.sky1.x += SKY_WIDTH * 2;
+      }
+      if (this.sky2.x < -SKY_WIDTH) {
+        return this.sky2.x += SKY_WIDTH * 2;
+      }
     };
-
     Game.prototype.check_grass = function() {
       this.grass1.x -= GRASS_SPEED;
       this.grass2.x -= GRASS_SPEED;
-      if (this.grass1.x < -GRASS_WIDTH) this.grass1.x += GRASS_WIDTH * 2;
-      if (this.grass2.x < -GRASS_WIDTH) return this.grass2.x += GRASS_WIDTH * 2;
+      if (this.grass1.x < -GRASS_WIDTH) {
+        this.grass1.x += GRASS_WIDTH * 2;
+      }
+      if (this.grass2.x < -GRASS_WIDTH) {
+        return this.grass2.x += GRASS_WIDTH * 2;
+      }
     };
-
     return Game;
-
-  })(GameState);
-
+  })();
   $(function() {
     var canvas, game, logo, stage;
     Ticker.setInterval(INTERVAL);
@@ -738,5 +627,4 @@
     logo.enter();
     return Ticker.addListener(logo);
   });
-
 }).call(this);
