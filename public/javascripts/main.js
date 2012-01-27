94c5664ace1d4a1e51db6fed15475888a5f732c3
Martin Stannard <mstannard@gmail.com>
2012-01-27 22:10:28 +1100

    added require.js javascript loading. Needed to replace underscore with a AMD compatible version from https://github.com/amdjs/underscore

---
+++ public/javascripts/main.js
@@ -0,0 +1,6 @@
+require(["order!vendor/easel", "order!vendor/jquery", "order!vendor/underscore-min", "order!app"], function(util) {
+    //This function is called when scripts/helper/util.js is loaded.
+    //If util.js calls define(), then this function is not fired until
+    //util's dependencies have loaded, and the util argument will hold
+    //the module value for "helper/util".
+});
